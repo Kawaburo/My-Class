f437a87a8b912702df20ca4c9fa374ff7cd3a5bf
Kawaburo <kawaburo@gmail.com>
2022-04-02 01:14:14 +0300

    add sort

--- src/Start/Main.java
+++ src/Start/Main.java
@@ -5,11 +5,13 @@ import Sort.MySort;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World");
 
         int [] newArray = MyArray.randomArray(10,50);
         MyArray.displayArray(newArray);
         System.out.println("");
         MySort.bobbleSort(newArray);
         MyArray.displayArray(newArray);
+
     }
 }
