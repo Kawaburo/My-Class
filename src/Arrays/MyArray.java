d6e8a00360f4482242c739a83d994d1fc39548fd
kawaburo <kawaburo@gmail.com>
2022-04-01 17:01:54 +0300

    commit

--- src/Arrays/MyArray.java
+++ src/Arrays/MyArray.java
@@ -2,24 +2,17 @@ package Arrays;
 
 public class MyArray {
 
-    public static int[] randomArray(int size,int maxVal){
-        int [] array = new int[size];
+    public static int[] randomArray(int size, int maxVal) {
+        int[] array = new int[size];
         for (int i = 0; i < array.length; i++) {
-            int randomVal = (int)(Math.random()*maxVal);
-            array[i]=randomVal;
+            int randomVal = (int) (Math.random() * maxVal);
+            array[i] = randomVal;
         }
         return array;
-    }
-    public static void displayArray(int [] array){
-        for(int i =0; i<array.length; i++) System.out.print(" "+array[i]);
     }
 
-    public static int [] dispEasyNumber(int [] array) {
-        for (int i = 0; i < array.length; i++) {
-            if((array[i] / array[i])==1){
-                System.out.println(array[i]);
-            }
-        }
-        return array;
+    public static void displayArray(int[] array) {
+        for (int i = 0; i < array.length; i++) System.out.print(" " + array[i]);
     }
+
 }
--- src/Start/Main.java
+++ src/Start/Main.java
@@ -4,9 +4,8 @@ import Arrays.MyArray;
 
 public class Main {
     public static void main(String[] args) {
-        int [] newArray = MyArray.randomArray(50,10000);
 
+        int [] newArray = MyArray.randomArray(50,10000);
         MyArray.displayArray(newArray);
-        MyArray.dispEasyNumber(newArray);
     }
 }
