cea5ab14aa53af4777125e3fdcb942032b070bd6
Kawaburo <kawaburo@gmail.com>
2022-04-22 09:22:22 +0300

    commit

--- src/Sort/MySort.java
+++ src/Sort/MySort.java
@@ -17,4 +17,27 @@ public class MySort {
         }
         return  bobbleSort;
     }
+    public static int [] bobbleSortDouble(int [] bobbleSortDouble){
+        boolean flag = false;
+        while (!flag){
+            flag=true;
+            for (int i = 0; i < bobbleSortDouble.length -1; i++) {
+                if(bobbleSortDouble[i]>bobbleSortDouble[i+1]){
+                    flag=false;
+                    int buf = bobbleSortDouble[i];
+                    bobbleSortDouble[i]=bobbleSortDouble[i+1];
+                    bobbleSortDouble[i+1]=buf;
+                }
+            }
+            for (int i = bobbleSortDouble.length+1; i == 0; i--) {
+                if(bobbleSortDouble[i]>bobbleSortDouble[i+1]){
+                    flag=false;
+                    int buf = bobbleSortDouble[i-1];
+                    bobbleSortDouble[i-1]=bobbleSortDouble[i];
+                    bobbleSortDouble[i-1]=buf;
+                }
+            }
+        }
+        return  bobbleSortDouble;
+    }
 }
--- src/Start/Main.java
+++ src/Start/Main.java
@@ -3,15 +3,18 @@ package Start;
 import Algorithms.Read;
 import Algorithms.Write;
 import Arrays.MyArray;
+import Sort.MySort;
 
 import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        String dest = "E:\\java\\My-Class\\files\\newArray";
-        int [] newArray = MyArray.inOrder(10,10);
-        Write.writeArray(dest,newArray);
-        Read.arrayList(dest);
-
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        long start = System.currentTimeMillis();
+        int [] newArray = MyArray.randomArray(1000000,10);
+        Thread.sleep(1000);
+        MySort.bobbleSort(newArray);
+        long finish = System.currentTimeMillis();
+        long elapsed = finish - start;
+        System.out.println("Прошло времени, мс: " + elapsed);
     }
 }
